633eae6e5f8418606bac040cb08a6d04bb457ff1
bhuvan243 <bhuvan243@gmail.com>
2024-01-28 22:18:19 +0530

    submitting code-Sun Jan 28 2024 22:18:18 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <ul>
+          <li><Link to={"/"}>Home</Link></li>
+          <li><Link to={"/about"}>About</Link></li>
+        </ul>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>,
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+  document.getElementById("root")
+);
